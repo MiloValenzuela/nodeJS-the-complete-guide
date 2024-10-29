9e406c5d625aee28ab01286dbf4f5409debc21f9
MiloValenzuela <valenzuelagarza@gmail.com>
2024-10-29 14:07:06 -0600

    Destructuring

--- JsRecap/play.js
+++ JsRecap/play.js
@@ -6,20 +6,31 @@ const person = {
     }
 };
 
-copiedPerson = {...person};
-console.log(copiedPerson);
+const printName = ({ name, age }) => {
+    console.log(name);
+}
+
+printName(person);
+
+const { name, age} = person;
+console.log(name, age);
+
+// copiedPerson = {...person};
+// console.log(copiedPerson);
 
 const hobbies = ['Sports', 'Cooking'];
-// for (let hobby of hobbies) {
-//     console.log(hobby);
-// }
-// console.log(hobbies.map( hobby => 'Hobby: ' + hobby));
-// console.log(hobbies);
-const copiedArray = [...hobbies];
-console.log(copiedArray);
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1, hobby2);
+// // for (let hobby of hobbies) {
+// //     console.log(hobby);
+// // }
+// // console.log(hobbies.map( hobby => 'Hobby: ' + hobby));
+// // console.log(hobbies);
+// const copiedArray = [...hobbies];
+// console.log(copiedArray);
 
-const toArray = (...args) => {
-    return args;
-};
+// const toArray = (...args) => {
+//     return args;
+// };
 
-console.log(toArray(1, 2, 3, 4));
+// console.log(toArray(1, 2, 3, 4));
