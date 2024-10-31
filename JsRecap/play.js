b945a5a350b753b34439ed1894f4673c071ee8e9
MiloValenzuela <valenzuelagarza@gmail.com>
2024-10-31 12:58:17 -0600

    Async & Promises

--- JsRecap/play.js
+++ JsRecap/play.js
@@ -1,15 +1,23 @@
 const fetchData = callback => {
-    setTimeout(() =>{
-        callback('Done!');
-    }, 1500);
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Done!');
+        }, 1500);    
+    });
+    return promise;
 };
 
 
 setTimeout(()=> {
     console.log('Time is done!');
-    fetchData(text => {
-       console.log(text); 
-    })
+    fetchData()
+        .then(text => {
+            console.log(text);
+            return fetchData();
+        });
+        fetchData().then(text2 => {
+            console.log(text2);
+    });
 }, 2000)
 
 console.log('Hello!');
